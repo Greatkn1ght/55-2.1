54dc4000bbaabf519da4b8fe444e9d7e280b8ed0
Greatkn1ght <greatkn1gh1@gmail.com>
2025-09-07 17:59:10 +0600

    Done 2

---
+++ films/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from datetime import datetime
+# Create your models here.
+
+class Director(models.Model):
+    fio = models.CharField(max_length=255)
+    birthday = models.DateField()
+
+    def __str__(self):
+        return self.fio
+    
+    def age(self):
+        now = datetime.now()
+        return now.year - self.birthday.year
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name 
+
+class Film(models.Model):
+    director = models.ForeignKey(Director, on_delete=models.CASCADE, null=True)
+    genres = models.ManyToManyField(Genre, blank=True)
+    title = models.CharField(max_length=256)
+    text = models.TextField(null=True, blank=True)
+    rate_kp = models.FloatField()
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+STARS = ((i, '*' * i) for i in range(1,6))
+
+class Review(models.Model):
+    text = models.TextField()
+    film = models.ForeignKey(Film, on_delete=models.CASCADE, related_name='reviews')
+    stars = models.IntegerField(choices=STARS, default=2)
+
+    def __str__(self):
+        return self.text
---
+++ product/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from .models import Category, Product, Review
+
+class CategoryDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+        
+class ProductDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class ReviewDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+class CategoryListSerializer(serializers.ModelSerializer):
+    products_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Category
+        fields = 'name products_count'.split()
+
+    def get_products_count(self, obj):
+        return obj.product_set.count()
+        
+class ProductListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = 'title description price category'.split()
+
+class ReviewListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = 'text stars'.split()
+
